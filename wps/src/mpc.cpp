bb3ebe7ee8cf658e66f7a67b1e6134720fd75f4c
Trent0881 <taz15@case.edu>
2017-02-11 13:13:50 -0500

    Initial improvemnts to path

--- wps/src/mpc.cpp
+++ wps/src/mpc.cpp
@@ -36,7 +36,7 @@ int main(int argc, char **argv) {
     //create some path points...this should be done by some intelligent algorithm, but we'll hard-code it here
     geometry_msgs::PoseStamped pose_stamped;
     geometry_msgs::Pose pose;
-    pose.position.x = 1.0; // say desired x-coord is 1
+    pose.position.x = 2; // say desired x-coord is inititally 2
     pose.position.y = 0.0;
     pose.position.z = 0.0; // let's hope so!
     pose.orientation.x = 0.0; //always, for motion in horizontal plane
@@ -48,21 +48,15 @@ int main(int argc, char **argv) {
     
     // Additional poses to push to service
     // 
-    quat = convertPlanarPhi2Quaternion(1.57); // get a quaternion corresponding to this heading
+    quat = convertPlanarPhi2Quaternion(1.57);
     pose_stamped.pose.orientation = quat;   
-    pose_stamped.pose.position.y=1.0; // say desired y-coord is 1.0
+    pose_stamped.pose.position.y=3;
     path_srv.request.nav_path.poses.push_back(pose_stamped);
 
     //
     quat = convertPlanarPhi2Quaternion(1.57); 
     pose_stamped.pose.orientation = quat;   
     pose_stamped.pose.position.y=1.0; 
-    path_srv.request.nav_path.poses.push_back(pose_stamped);
-
-    //
-    quat = convertPlanarPhi2Quaternion(1.57);
-    pose_stamped.pose.orientation = quat;   
-    pose_stamped.pose.position.y=1.0;
     path_srv.request.nav_path.poses.push_back(pose_stamped);
     
     // Finally ask the server to initiate that path
