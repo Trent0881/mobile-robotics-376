2b1f9a349c51682f6a26bc03c0a01c3f7a1827a5
Trent0881 <taz15@case.edu>
2017-02-22 09:08:53 -0500

    as is now as not ac

--- pas/src/as.cpp
+++ pas/src/as.cpp
@@ -1,69 +1,115 @@
-// example_action_client: 
-// wsn, October, 2014
+// Path Action Server
+// Written by Trent Ziemer 2/21/2017, heavily based on work from Dr. Wyatt Newman
 
-#include<ros/ros.h>
+#include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
+#include <example_action_server/demoAction.h>
 
-//this #include refers to the new "action" message defined for this package
-// the action message can be found in: .../example_action_server/action/demo.action
-// automated header generation creates multiple headers for message I/O
-// these are referred to by the root name (demo) and appended name (Action)
-// If you write a new client of the server in this package, you will need to include example_action_server in your package.xml,
-// and include the header file below
-#include<example_action_server/demoAction.h>
+int g_count = 0;
+bool g_count_failure = false;
 
+class ExampleActionServer {
+private:
 
-// This function will be called once when the goal completes
-// this is optional, but it is a convenient way to get access to the "result" message sent by the server
+    ros::NodeHandle nh_;  // we'll need a node handle; get one upon instantiation
 
-void doneCb(const actionlib::SimpleClientGoalState& state,
-        const example_action_server::demoResultConstPtr& result) {
-    ROS_INFO(" doneCb: server responded with state [%s]", state.toString().c_str());
-    int diff = result->output - result->goal_stamp;
-    ROS_INFO("got result output = %d; goal_stamp = %d; diff = %d", result->output, result->goal_stamp, diff);
+    // this class will own a "SimpleActionServer" called "as_".
+    // it will communicate using messages defined in example_action_server/action/demo.action
+    // the type "demoAction" is auto-generated from our name "demo" and generic name "Action"
+    actionlib::SimpleActionServer<example_action_server::demoAction> as_;
+    
+    // here are some message types to communicate with our client(s)
+    example_action_server::demoGoal goal_; // goal message, received from client
+    example_action_server::demoResult result_; // put results here, to be sent back to the client when done w/ goal
+    example_action_server::demoFeedback feedback_; // not used in this example; 
+    // would need to use: as_.publishFeedback(feedback_); to send incremental feedback to the client
+
+public:
+    ExampleActionServer(); //define the body of the constructor outside of class definition
+
+    ~ExampleActionServer(void) {
+    }
+    // Action Interface
+    void executeCB(const actionlib::SimpleActionServer<example_action_server::demoAction>::GoalConstPtr& goal);
+};
+
+//implementation of the constructor:
+// member initialization list describes how to initialize member as_
+// member as_ will get instantiated with specified node-handle, name by which this server will be known,
+//  a pointer to the function to be executed upon receipt of a goal.
+//  
+// Syntax of naming the function to be invoked: get a pointer to the function, called executeCB, which is a member method
+// of our class exampleActionServer.  Since this is a class method, we need to tell boost::bind that it is a class member,
+// using the "this" keyword.  the _1 argument says that our executeCB takes one argument
+// the final argument  "false" says don't start the server yet.  (We'll do this in the constructor)
+
+ExampleActionServer::ExampleActionServer() :
+   as_(nh_, "example_action", boost::bind(&ExampleActionServer::executeCB, this, _1),false) 
+// in the above initialization, we name the server "example_action"
+//  clients will need to refer to this name to connect with this server
+{
+    ROS_INFO("in constructor of exampleActionServer...");
+    // do any other desired initializations here...specific to your implementation
+
+    as_.start(); //start the server running
+}
+
+//executeCB implementation: this is a member method that will get registered with the action server
+// argument type is very long.  Meaning:
+// actionlib is the package for action servers
+// SimpleActionServer is a templated class in this package (defined in the "actionlib" ROS package)
+// <example_action_server::demoAction> customizes the simple action server to use our own "action" message 
+// defined in our package, "example_action_server", in the subdirectory "action", called "demo.action"
+// The name "demo" is prepended to other message types created automatically during compilation.
+// e.g.,  "demoAction" is auto-generated from (our) base name "demo" and generic name "Action"
+void ExampleActionServer::executeCB(const actionlib::SimpleActionServer<example_action_server::demoAction>::GoalConstPtr& goal) {
+    //ROS_INFO("in executeCB");
+    //ROS_INFO("goal input is: %d", goal->input);
+    //do work here: this is where your interesting code goes
+    
+    //....
+
+    // for illustration, populate the "result" message with two numbers:
+    // the "input" is the message count, copied from goal->input (as sent by the client)
+    // the "goal_stamp" is the server's count of how many goals it has serviced so far
+    // if there is only one client, and if it is never restarted, then these two numbers SHOULD be identical...
+    // unless some communication got dropped, indicating an error
+    // send the result message back with the status of "success"
+
+    g_count++; // keep track of total number of goals serviced since this server was started
+    result_.output = g_count; // we'll use the member variable result_, defined in our class
+    result_.goal_stamp = goal->input;
+    
+    // the class owns the action server, so we can use its member methods here
+   
+    // DEBUG: if client and server remain in sync, all is well--else whine and complain and quit
+    // NOTE: this is NOT generically useful code; server should be happy to accept new clients at any time, and
+    // no client should need to know how many goals the server has serviced to date
+    if (g_count != goal->input) {
+        ROS_WARN("hey--mismatch!");
+        ROS_INFO("g_count = %d; goal_stamp = %d", g_count, result_.goal_stamp);
+        g_count_failure = true; //set a flag to commit suicide
+        ROS_WARN("informing client of aborted goal");
+        as_.setAborted(result_); // tell the client we have given up on this goal; send the result message as well
+    }
+    else {
+         as_.setSucceeded(result_); // tell the client that we were successful acting on the request, and return the "result" message
+    }
 }
 
 int main(int argc, char** argv) {
-    ros::init(argc, argv, "demo_action_client_node"); // name this node 
-    int g_count = 0;
-    // here is a "goal" object compatible with the server, as defined in example_action_server/action
-    example_action_server::demoGoal goal;
+    ros::init(argc, argv, "demo_action_server_node"); // name this node 
 
-    // use the name of our server, which is: example_action (named in example_action_server.cpp)
-    // the "true" argument says that we want our new client to run as a separate thread (a good idea)
-    actionlib::SimpleActionClient<example_action_server::demoAction> action_client("example_action", true);
+    ROS_INFO("instantiating the demo action server: ");
 
-    // attempt to connect to the server:
-    ROS_INFO("waiting for server: ");
-    bool server_exists = action_client.waitForServer(ros::Duration(5.0)); // wait for up to 5 seconds
-    // something odd in above: does not seem to wait for 5 seconds, but returns rapidly if server not running
-    //bool server_exists = action_client.waitForServer(); //wait forever
-
-    if (!server_exists) {
-        ROS_WARN("could not connect to server; halting");
-        return 0; // bail out; optionally, could print a warning message and retry
-    }
-
-
-    ROS_INFO("connected to action server"); // if here, then we connected to the server;
-
-    while (true) {
-        // stuff a goal message:
-        g_count++;
-        goal.input = g_count; // this merely sequentially numbers the goals sent
-        //action_client.sendGoal(goal); // simple example--send goal, but do not specify callbacks
-        action_client.sendGoal(goal, &doneCb); // we could also name additional callback functions here, if desired
-        //    action_client.sendGoal(goal, &doneCb, &activeCb, &feedbackCb); //e.g., like this
-
-        bool finished_before_timeout = action_client.waitForResult(ros::Duration(5.0));
-        //bool finished_before_timeout = action_client.waitForResult(); // wait forever...
-        if (!finished_before_timeout) {
-            ROS_WARN("giving up waiting on result for goal number %d", g_count);
-            return 0;
-        } else {
-            //if here, then server returned a result to us
-        }
-
+    ExampleActionServer as_object; // create an instance of the class "ExampleActionServer"
+    
+    ROS_INFO("going into spin");
+    // from here, all the work is done in the action server, with the interesting stuff done within "executeCB()"
+    // you will see 5 new topics under example_action: cancel, feedback, goal, result, status
+    while (!g_count_failure) {
+        ros::spinOnce(); //normally, can simply do: ros::spin();  
+        // for debug, induce a halt if we ever get our client/server communications out of sync
     }
 
     return 0;
